97d98373e428842802ca0e5bec9e1ff9cdeba682
jagersd <jagersd@gmail.com>
2022-06-05 14:37:28 +0200

    Muumin theme added

---
+++ polybar/muumin/launch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+
+MONITORPRIMARY="$(xrandr --query | grep " primary" | cut -d" " -f1)";
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [ "$MONITORPRIMARY" = "$m" ]; then
+        MONITOR=$m polybar --config=~/.config/polybar/muumin/config --reload muumin &
+    else
+        MONITOR=$m polybar --config=~/.config/polybar/muumin/config --reload others &
+    fi
+  done
+else
+  polybar --reload muumin &
+fi
+
