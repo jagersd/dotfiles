468fba4eb2ea1f2b0b6b9de369fee48ca88dad11
stefan <s.jager@live.nl>
2024-11-09 18:03:50 +0100

    trying out nushell

--- general/nvim/lua/plugins/treesitter.lua
+++ general/nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "go", "lua", "php", "vue", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
+        local configs = require("nvim-treesitter.configs")
+        dependencies = {
+            { "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
+        },
+        configs.setup({
+            ensure_installed = { "go", "lua", "php", "vue", "javascript", "html" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },  
         })
     end
- }
+}
