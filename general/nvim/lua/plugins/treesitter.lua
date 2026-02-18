8c093ff229fb7efd1e801a92727fc071c8bfd49f
Stefan de Jager <jagersd@gmail.com>
2026-02-18 18:06:10 +0100

    Add alloy to nvim setup

--- general/nvim/lua/plugins/treesitter.lua
+++ general/nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,15 @@ return {
     build = ":TSUpdate",
     config = function () 
         local configs = require("nvim-treesitter.configs")
-        dependencies = {
-            { "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.alloy = {
+        install_info = {
+            url = "https://github.com/mattsre/tree-sitter-alloy",
+            files = { "src/parser.c" },
+            branch = "main",
         },
+        filetype = "alloy",
+        }
         configs.setup({
             ensure_installed = { "go", "lua", "php", "javascript", "html" },
             sync_install = false,
@@ -14,3 +20,4 @@ return {
         })
     end
 }
+
--- general/nvim/lua/vars.lua
+++ general/nvim/lua/vars.lua
@@ -4,7 +4,7 @@ vim.cmd.syntax("on")
 local tabGrp = vim.api.nvim_create_augroup("Markdown per filetype", { clear = true })
 
 vim.api.nvim_create_autocmd('BufEnter', {
-    pattern = {'*.yml,*.yaml,*.ini,*.toml'},
+    pattern = {'*.yml,*.yaml,*.ini,*.toml,*.alloy'},
     group = tabGrp,
     command = 'setlocal shiftwidth=2 softtabstop=2'
 })
